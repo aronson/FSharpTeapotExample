9fddb8489cc5ef6447547053dca8f41665c1edb7
Isaac Aronson <i@pingas.org>
2021-09-06 16:09:01 -0500

    Render a triangle

--- Program.fs
+++ Program.fs
@@ -1,25 +1,98 @@
 open Silk.NET.Windowing
+open System.Threading
+open Silk.NET.OpenGL
+open Microsoft.FSharp.NativeInterop
+open System
+open System.IO
+open System.Drawing
+
+let compileShader (shader: uint32) (gl: GL) =
+  gl.CompileShader(shader)
+  let code = gl.GetShader(shader, GLEnum.CompileStatus)
+  if code <> int GLEnum.True then failwith (sprintf "Error compiling shader %d" shader)
+
+let linkProgram (program: uint32) (gl: GL) =
+  gl.LinkProgram(program)
+  let code = gl.GetProgram(program, GLEnum.LinkStatus)
+  if code <> int GLEnum.True then failwith (sprintf "Error linking program %d" program)
+
+let createShader vertPath fragPath (gl: GL) =
+  let vertexShaderSource = File.ReadAllText vertPath
+  let fragmentShaderSource = File.ReadAllText fragPath
+  let vertexShader = gl.CreateShader(GLEnum.VertexShader)
+  let fragmentShader = gl.CreateShader(GLEnum.FragmentShader)
+  gl.ShaderSource(vertexShader, vertexShaderSource)
+  gl.ShaderSource(fragmentShader, fragmentShaderSource)
+  compileShader vertexShader gl
+  compileShader fragmentShader gl
+
+  let handle = gl.CreateProgram()
+  gl.AttachShader(handle, vertexShader)
+  gl.AttachShader(handle, fragmentShader)
+
+  linkProgram handle gl
+
+  gl.DetachShader(handle, vertexShader)
+  gl.DetachShader(handle, fragmentShader)
+  gl.DeleteShader(vertexShader)
+  gl.DeleteShader(fragmentShader)
+
+  handle
 
 [<EntryPoint>]
-let main argv =
+let main _ =
+    let vertices = [|
+        -0.5f; -0.5f; 0.0f // Bottom-left vertex
+        0.5f; -0.5f; 0.0f  // Bottom-right vertex
+        0.0f;  0.5f; 0.0f  // Top vertex
+    |]
+
     let mutable options = WindowOptions.Default
     options.UpdatesPerSecond <- 60.0
     options.FramesPerSecond <- 60.0
 
-    let mutable window = Window.Create(options)
+    let window = Window.Create(options)
+    window.Title <- "Luigi #1"
 
-    window.add_FileDrop (fun files -> files |> Seq.iter (fun file -> printfn "%s" file))
-    window.add_Move (fun position -> printfn "Moving %A" position)
-    window.add_Resize (fun size -> printfn "Resize %A" size)
-    window.add_StateChanged (fun state -> printfn "%A" state)
-    window.add_Load (fun () -> printfn "Finished loading")
-    window.add_Closing (fun () -> printfn "Window is closing now")
-    window.add_FocusChanged (fun isFocused -> printfn "Focused = %b" isFocused)
-    window.add_Render (fun delta -> printfn "Render %A" (1.0 / delta))
-    window.add_Update (fun delta -> printfn "Update %A" (1.0 / delta))
+    window.add_Load (fun () -> 
+        let gl = GL.GetApi(window)
 
-    window.VSync <- true
+        let vertexBufferObject = gl.GenBuffer()
+        let vertexArrayObject = gl.GenVertexArray()
+        let shaderHandle = createShader "shader.vert" "shader.frag" gl
 
+        gl.ClearColor(0.2f, 0.3f, 0.3f, 1.0f)
+        gl.BindBuffer(GLEnum.ArrayBuffer, vertexBufferObject)
+        use floatPtr = fixed vertices
+        let voidPtr = floatPtr |> NativePtr.toVoidPtr
+        let size = unativeint (vertices.Length * sizeof<float32>)
+        gl.BufferData(GLEnum.ArrayBuffer, size, voidPtr, GLEnum.StaticDraw)
+
+        gl.UseProgram(shaderHandle)
+        gl.BindVertexArray(vertexArrayObject)
+        gl.VertexAttribPointer(0u, 3, GLEnum.Float, false, uint32 (3 * sizeof<float32>), IntPtr.Zero.ToPointer())
+        gl.EnableVertexAttribArray(0u)
+        gl.BindBuffer(GLEnum.ArrayBuffer, vertexBufferObject)
+
+        window.add_Render(fun _ -> 
+            gl.Clear(uint32 GLEnum.ColorBufferBit)
+            gl.UseProgram(shaderHandle)
+            gl.BindVertexArray(vertexArrayObject)
+            gl.DrawArrays(GLEnum.Triangles, 0, 3u)
+        )
+
+        window.add_Resize(fun size ->
+            gl.Viewport(size)
+        )
+
+        window.add_Closing(fun () -> 
+            gl.BindBuffer(GLEnum.ArrayBuffer, 0u)
+            gl.BindVertexArray(0u)
+            gl.UseProgram(0u)
+            gl.DeleteBuffer(vertexBufferObject)
+            gl.DeleteVertexArray(vertexArrayObject)
+            gl.DeleteProgram(shaderHandle)
+        )
+    )
     window.Run()
-
     0 // exit sucessfully
--- Teapot.fs
+++ Teapot.fs
@@ -1,4 +1,4 @@
-module VertexData
+module Data
 
 let teapot =
     [ 0.700000f,
