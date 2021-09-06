51651b411eea62ddcd0126ef83bc102f28b7ad8b
Isaac Aronson <i@pingas.org>
2021-09-06 14:45:37 -0500

    Steal some window rendering event code

--- Program.fs
+++ Program.fs
@@ -1,4 +1,25 @@
-open System
+open Silk.NET.Windowing
 
 [<EntryPoint>]
-let main argv = 0 // exit sucessfully
+let main argv =
+    let mutable options = WindowOptions.Default
+    options.UpdatesPerSecond <- 60.0
+    options.FramesPerSecond <- 60.0
+
+    let mutable window = Window.Create(options)
+
+    window.add_FileDrop (fun files -> files |> Seq.iter (fun file -> printfn "%s" file))
+    window.add_Move (fun position -> printfn "Moving %A" position)
+    window.add_Resize (fun size -> printfn "Resize %A" size)
+    window.add_StateChanged (fun state -> printfn "%A" state)
+    window.add_Load (fun () -> printfn "Finished loading")
+    window.add_Closing (fun () -> printfn "Window is closing now")
+    window.add_FocusChanged (fun isFocused -> printfn "Focused = %b" isFocused)
+    window.add_Render (fun delta -> printfn "Render %A" (1.0 / delta))
+    window.add_Update (fun delta -> printfn "Update %A" (1.0 / delta))
+
+    window.VSync <- true
+
+    window.Run()
+
+    0 // exit sucessfully
